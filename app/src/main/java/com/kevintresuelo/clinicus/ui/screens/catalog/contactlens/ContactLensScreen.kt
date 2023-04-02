9bc921f6b3d97ff54d0ce530f847c01ec11d9d38
Kevin Tresuelo <kevztresuelo@gmail.com>
2023-04-02 17:28:11 +0800

    feat: add button
    Took 11 minutes

--- app/src/main/java/com/kevintresuelo/clinicus/ui/screens/catalog/contactlens/ContactLensScreen.kt
+++ app/src/main/java/com/kevintresuelo/clinicus/ui/screens/catalog/contactlens/ContactLensScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -107,6 +108,15 @@ fun ContactLensPower() {
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
                 singleLine = true,
             )
+        }
+
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth(),
+            enabled = spectacleSphere.isNotEmpty() || spectacleCylinder.isNotEmpty(),
+        ) {
+            Text(text = stringResource(id = AppStrings.tools_contact_lens_calculate))
         }
 
     }
